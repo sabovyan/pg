32bc08ed778615d047b6462b6cfa6fd2124841e8
sabovyan <abovyan.sargis90@gmail.com>
2020-10-02 18:04:54 +0400

    ci(person) add perosn exersize

---
+++ Person/person.js
@@ -0,0 +1,35 @@
+let timerOneID, timerTwoID;
+
+const { faker } = window;
+
+class Person {
+  constructor() {
+    this.name = faker.name.firstName();
+    this.age = faker.random.number({ min: 1, max: 50 });
+    this.incAge();
+  }
+  incAge() {
+    this.age += 1;
+    setTimeout(this.incAge.bind(this), 1000);
+  }
+}
+
+const per = new Person();
+
+let people = [];
+for (let i = 0; i < 4; i += 1) {
+  people.push(new Person());
+}
+
+filterByAge = (limitedAge) => people.filter(({ age }) => age < limitedAge - 1);
+
+setInterval(() => {
+  people = filterByAge(40);
+  console.log(people);
+}, 1000);
+
+function addNewPeople() {
+  setInterval(() => {
+    people.push(new Person());
+  }, 1000);
+}
